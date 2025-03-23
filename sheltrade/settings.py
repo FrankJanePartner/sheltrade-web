04b3cbac6c31d4d9907d4d177e543432ef6f09d3
FrankJanePartner <partnermarvel55@gmail.com>
2025-03-23 20:16:03 +0100

    upadated custom middleware path, add django tinymce library to installed apps and its configuraations

--- sheltrade/settings.py
+++ sheltrade/settings.py
@@ -70,6 +70,7 @@ INSTALLED_APPS = [
     'django_countries',
     'phonenumber_field',
     'mptt',
+    'tinymce',
     # 'guardian',
     'rest_framework',
     'rest_framework.authtoken',
@@ -91,7 +92,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
     # my apps middleware
-    'billPayments.middleware.RequestMiddleware',
+    'core.middleware.RequestMiddleware',
 
     # allauth specific middleware
     'allauth.account.middleware.AccountMiddleware',
@@ -360,3 +361,31 @@ ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL = "/dashboard/"
 
 # API confirmation
 # DJRESTAUTH_VERIFICATION_URL = '/accounts/login/'
+
+
+TINYMCE_DEFAULT_CONFIG = {
+    'cleanup_on_startup': True,
+    'custom_undo_redo_levels': 20,
+    'selector': 'textarea',
+    'theme': 'silver',
+    'plugins': '''
+            textcolor save link image media preview codesample contextmenu
+            table code lists fullscreen  insertdatetime  nonbreaking
+            contextmenu directionality searchreplace wordcount visualblocks
+            visualchars code fullscreen autolink lists  charmap print  hr
+            anchor pagebreak
+            ''',
+    'toolbar1': '''
+            fullscreen preview bold italic underline | fontselect,
+            fontsizeselect  | forecolor backcolor | alignleft alignright |
+            aligncenter alignjustify | indent outdent | bullist numlist table |
+            | link image media | codesample |
+            ''',
+    'toolbar2': '''
+            visualblocks visualchars |
+            charmap hr pagebreak nonbreaking anchor |  code |
+            ''',
+    'contextmenu': 'formats | link image',
+    'menubar': True,
+    'statusbar': True,
+}
