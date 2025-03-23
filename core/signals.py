586c46d6d92e089fd47d74e492fe6b98dc510fc8
FrankJanePartner <partnermarvel55@gmail.com>
2025-03-23 20:20:25 +0100

    Update core app signals.py for user group management

--- core/signals.py
+++ core/signals.py
@@ -1,25 +1,26 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User
-from .models import Profile, Notification
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 from django.contrib.auth.models import User, Group
-from .models import Profile, Notification
+from .models import Profile
+from .middleware import RequestMiddleware
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+    request = RequestMiddleware.get_request()
+
     if created:
         # Create a profile instance for the new user
-        Profile.objects.create(user=instance)        
+        Profile.objects.create(user=instance)
 
-
-@receiver(post_save, sender=User)
-def assign_user_group(sender, instance, created, **kwargs):
-
-    if instance.is_staff and not instance.is_superuser:
-        worker_group, _ = Group.objects.get_or_create(name='Workers')
-        instance.groups.add(worker_group)
-    elif not instance.is_staff and not instance.is_superuser:
-        customer_group, _ = Group.objects.get_or_create(name='Customers')
-        instance.groups.add(customer_group)
+        if request and request.path.startswith("/admin/auth/user/add/"):
+            instance.is_staff = True  # Ensure staff status
+            instance.save(update_fields=["is_staff"])  # Save only is_staff to avoid overriding other fields
+        
+        # Assign user to appropriate group
+        if instance.is_staff and not instance.is_superuser:
+            worker_group, _ = Group.objects.get_or_create(name="Workers")
+            instance.groups.add(worker_group)
+        elif not instance.is_staff and not instance.is_superuser:
+            customer_group, _ = Group.objects.get_or_create(name="Customers")
+            instance.groups.add(customer_group)
