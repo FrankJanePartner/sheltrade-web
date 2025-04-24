26d8b9c4ab8ef978d66ddfddd24084de7e1491e4
Frank Jane Partner <96851698+FrankJanePartner@users.noreply.github.com>
2025-04-24 21:47:49 +0100

    Update urls.py

--- legal/urls.py
+++ legal/urls.py
@@ -4,5 +4,5 @@ from .views import terms_conditions
 
 app_name = "legal"
 urlpatterns = [
-    path('', terms_conditions, name='terms_conditions'),
+    path('<slug:slug>', terms_conditions, name='terms_conditions'),
 ]
